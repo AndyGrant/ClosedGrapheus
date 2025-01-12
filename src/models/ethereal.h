30e368ae2074bfc3e4dcf100bb50aea25ccff899
Andrew Grant <andrew.github@grantnet.us>
2025-01-12 17:50:54 -0500

    ....

--- src/models/ethereal.h
+++ src/models/ethereal.h
@@ -39,24 +39,19 @@ namespace model {
 
     struct EtherealModel : ChessModel {
 
-        SparseInput *halfkp1, *halfkp2;
+        SparseInput *half1, *half2;
 
-        // Defines the halfkp + virtual PSQT realtions
-
-        const size_t n_king_buckets  = 32;
-        const size_t n_relations     = 10;
         const size_t n_squares       = 64;
+        const size_t n_piece_types   = 6;
+        const size_t n_colours       = 2;
 
-        const size_t n_real_features    = n_king_buckets * n_relations * n_squares;
-        const size_t n_virtual_features = n_relations * n_squares;
-        const size_t n_features         = n_real_features + n_virtual_features;
+        const size_t n_features      = n_squares * n_piece_types * n_colours;
 
         // Defines the sizes of the Network's Layers
 
         const size_t n_l0 = 768;
-        const size_t n_l1 = 8;
-        const size_t n_l2 = 32;
-        const size_t n_l3 = 1;
+        const size_t n_l1 = 32;
+        const size_t n_l2 = 1;
 
         // Defines miscellaneous hyper-parameters
 
@@ -67,9 +62,8 @@ namespace model {
         // Defines the mechanism of Quantization
 
         const size_t quant_ft = 64; // x64
-        const size_t quant_l1 = 32; // x32
-        const size_t quant_l2 = 1;  // None
-        const size_t quant_l3 = 1;  // None
+        const size_t quant_l1 = 64; // x32
+        const size_t quant_l2 = 64; // None
 
         const double clip_l1  = 127.0 / quant_l1;
 
@@ -82,10 +76,10 @@ namespace model {
 
         EtherealModel(size_t save_rate = 50) : ChessModel(0) /* TODO: Fix unneeded Lambda */ {
 
-            halfkp1 = add<SparseInput>(n_features, 60); // Real + Virtual
-            halfkp2 = add<SparseInput>(n_features, 60); // Real + Virtual
+            half1 = add<SparseInput>(n_features, 32); // Real + Virtual
+            half2 = add<SparseInput>(n_features, 32); // Real + Virtual
 
-            auto ft  = add<FeatureTransformer>(halfkp1, halfkp2, n_l0);
+            auto ft  = add<FeatureTransformer>(half1, half2, n_l0);
             ft->ft_regularization  = 1.0 / 16384.0 / 4194304.0;
 
             auto fta = add<ClippedRelu>(ft);
@@ -95,19 +89,15 @@ namespace model {
             auto l1a = add<ReLU>(l1);
 
             auto l2  = add<Affine>(l1a, n_l2);
-            auto l2a = add<ReLU>(l2);
-
-            auto l3  = add<Affine>(l2a, n_l3);
-            auto l3a = add<Sigmoid>(l3, sigm_coeff);
+            auto l2a = add<Sigmoid>(l2, sigm_coeff);
 
             set_save_frequency(save_rate);
 
             add_optimizer(
                 AdamWarmup({
                     {OptimizerEntry {&ft->weights}}, {OptimizerEntry {&ft->bias}},
-                    {OptimizerEntry {&l1->weights}.clamp(-clip_l1, clip_l1)}, {OptimizerEntry {&l1->bias}},
+                    {OptimizerEntry {&l1->weights}}, {OptimizerEntry {&l1->bias}},
                     {OptimizerEntry {&l2->weights}}, {OptimizerEntry {&l2->bias}},
-                    {OptimizerEntry {&l3->weights}}, {OptimizerEntry {&l3->bias}}
                 }, adam_beta1, adam_beta2, adam_eps, adam_warmup)
             );
 
@@ -115,54 +105,36 @@ namespace model {
                 "quant", save_rate,
                 QuantizerEntry<int16_t>(&ft->weights.values, quant_ft),
                 QuantizerEntry<int16_t>(&ft->bias.values,    quant_ft),
-                QuantizerEntry<int8_t >(&l1->weights.values, quant_l1),
-                QuantizerEntry<int32_t>(&l1->bias.values,    quant_l1),
-                QuantizerEntry<float  >(&l2->weights.values, quant_l2),
-                QuantizerEntry<float  >(&l2->bias.values,    quant_l2),
-                QuantizerEntry<float  >(&l3->weights.values, quant_l3),
-                QuantizerEntry<float  >(&l3->bias.values,    quant_l3),
+                QuantizerEntry<int16_t>(&l1->weights.values, quant_l1),
+                QuantizerEntry<int16_t>(&l1->bias.values,    quant_l1),
+                QuantizerEntry<int16_t>(&l2->weights.values, quant_l2),
+                QuantizerEntry<int16_t>(&l2->bias.values,    quant_l2),
             });
         }
 
-        std::pair<int, int> ft_index(chess::Square pc_sq, chess::Piece pc, chess::Square k_sq, chess::Color view) {
+        int ft_index(chess::Square pc_sq, chess::Piece pc, chess::Color view) {
 
-            chess::PieceType piece_type  = chess::type_of(pc);
-            chess::Color     piece_color = chess::color_of(pc);
+            const chess::PieceType piece_type  = chess::type_of(pc);
+            const chess::Color     piece_color = chess::color_of(pc);
+            const chess::Square    rel_sq      = relative_square(view, pc_sq);
 
-            chess::Square rel_k_sq  = relative_square(view, k_sq);
-            chess::Square rel_pc_sq = relative_square(view, pc_sq);
-
-            if (queen_side_sq(rel_k_sq))
-                rel_k_sq = mirror_square(rel_k_sq), rel_pc_sq = mirror_square(rel_pc_sq);
-
-            int ft_real = n_squares * n_relations * sq64_to_sq32(rel_k_sq)
-                        + n_squares * (5 * (view == piece_color) + piece_type)
-                        + rel_pc_sq;
-
-            int ft_virtual = n_squares * (5 * (view == piece_color) + piece_type)
-                           + rel_pc_sq;
-
-            return std::make_pair(ft_real, ft_virtual + n_real_features);
+            return (piece_color == view) * n_squares * n_piece_types
+                 +  piece_type * n_squares
+                 +  rel_sq;
         }
 
         void setup_inputs_and_outputs(dataset::DataSet<chess::Position>* positions) {
 
-            halfkp1->sparse_output.clear();
-            halfkp2->sparse_output.clear();
+            half1->sparse_output.clear();
+            half2->sparse_output.clear();
 
             auto& target = m_loss->target;
 
-            #pragma omp parallel for schedule(static) num_threads(4)
+            #pragma omp parallel for schedule(static) num_threads(8)
             for (int b = 0; b < positions->header.entry_count; b++) {
 
                 chess::Position* pos = &positions->positions[b];
                 chess::Color     stm = pos->m_meta.stm();
-
-                chess::Square wking = pos->get_king_square<chess::WHITE>();
-                chess::Square bking = pos->get_king_square<chess::BLACK>();
-
-                chess::Square stm_king  = stm == chess::WHITE ? wking : bking;
-                chess::Square nstm_king = stm == chess::WHITE ? bking : wking;
 
                 chess::BB bb { pos->m_occupancy };
 
@@ -171,17 +143,8 @@ namespace model {
                     chess::Square sq = chess::lsb(bb);
                     chess::Piece  pc = pos->m_pieces.get_piece(index);
 
-                    if (chess::type_of(pc) != chess::KING) {
-
-                        auto [stm_real,  stm_virtual ] = ft_index(sq, pc, stm_king, stm);
-                        auto [nstm_real, nstm_virtual] = ft_index(sq, pc, nstm_king, !stm);
-
-                        halfkp1->sparse_output.set(b, stm_real);
-                        halfkp1->sparse_output.set(b, stm_virtual);
-
-                        halfkp2->sparse_output.set(b, nstm_real);
-                        halfkp2->sparse_output.set(b, nstm_virtual);
-                    }
+                    half1->sparse_output.set(b, ft_index(sq, pc, stm));
+                    half2->sparse_output.set(b, ft_index(sq, pc, !stm));
 
                     bb = chess::lsb_reset(bb);
                 }
